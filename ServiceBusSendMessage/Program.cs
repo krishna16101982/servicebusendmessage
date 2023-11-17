af64d49dabe12b1b6f46dcd3f4ba73f0caf78f29
Kumar Gaurav <krishna16101982@gmail.com>
2023-11-17 14:40:27 +0530

    first commit

---
+++ ServiceBusSendMessage/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System.Text;
+
+class Program
+{
+    const string ServiceBusConnectionString = "Endpoint=sb://azservicebus001.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=KcEVSrHaF5Pcs3ns5TvnJWTdA7s36Wn7K+ASbMmAkGY=";
+    const string QueueName = "servicebusqueue001";
+
+    static async Task Main(string[] args)
+    {
+        IQueueClient queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+        try
+        {
+            // Create a message
+            string messageBody = "Hello from Azure Service Bus!";
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            // Send the message to the queue
+            await queueClient.SendAsync(message);
+            Console.WriteLine("Message sent to the queue!");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+        }
+        finally
+        {
+            await queueClient.CloseAsync();
+        }
+    }
+}
